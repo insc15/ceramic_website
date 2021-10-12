7c814f4246707209a580a22828cbbe03fce8aa0a
Minh Hieu <ngohieu20031@gmail.com>
2021-10-12 16:21:16 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function(){ $(".page-header").load("components/header.html") });
 
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+        var banner = new Splide( '#banner' ,{type:'fade',heightRatio: 0.45,rewind:true,drag:false,autoplay:true,pauseOnHover:false,pagination:false,arrows:false}).mount();
+    }
+  }
+
 $(function() {
-    var banner = new Splide( '#banner' ,{type:'fade',heightRatio: 0.51,rewind:true,drag:false}).mount();
+   
 });
