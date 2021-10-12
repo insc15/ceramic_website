ed59835205d7b1b713e841b468cfb19fb652a638
Minh Hieu <ngohieu20031@gmail.com>
2021-10-12 15:31:14 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(function(){ $(".page-header").load("components/header.html") });
+
+$(function() {
+    var banner = new Splide( '#banner' ,{type:'fade',heightRatio: 0.51,rewind:true,drag:false}).mount();
+});
