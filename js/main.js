bf6d5ffe5b57abdd9b161dee881d77a4cee0ace4
Minh Hieu <ngohieu20031@gmail.com>
2021-10-14 13:27:36 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
+function gdriveimg(url) {
+  return "https://drive.google.com/uc?id="+url;
+} 
+
+function load_img(){
+  $("img[gsrc]").val(function() {
+    $(this).attr("src","https://drive.google.com/uc?id=" + $(this).attr("gsrc"));
+  });
+}
+
 $(function(){ $(".page-header").load("components/header.html") });
 
 document.onreadystatechange = function () {
-    if (document.readyState === 'complete') {
-        var banner = new Splide( '#banner' ,{type:'fade',heightRatio: 0.45,rewind:true,drag:false,autoplay:true,pauseOnHover:false,pagination:false,arrows:false}).mount();
-    }
+  if (document.readyState === 'complete') {
+      var banner = new Splide( '#banner' ,{type:'fade',heightRatio: 0.45,rewind:true,drag:false,autoplay:true,pauseOnHover:false,pagination:false,arrows:false}).mount();
+      load_img();
   }
-
-$(function() {
-   
-});
+}
