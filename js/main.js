d20bc02149ba8165d2baf41b14b82e2997774aaa
Minh Hieu <ngohieu20031@gmail.com>
2021-10-15 22:21:25 +0700

    UPDATE

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-function gdriveimg(url) {
-  return "https://drive.google.com/uc?id="+url;
-} 
+function load_page(s){
+  window.location.href = s+".html";
+}
 
 function load_img(){
   $("img[gsrc]").val(function() {
@@ -8,11 +8,43 @@ function load_img(){
   });
 }
 
+function toogle_side_nav(){
+  if ($(".side_nav").hasClass('hide-menu')) {      
+    $(".side_nav").removeClass('hide-menu');
+    $(".side_nav").addClass('show-menu');
+    $("body").css("overflow","hidden");
+  }else if ($(".side_nav").hasClass('show-menu')) {
+    $(".side_nav").removeClass('show-menu');
+    $(".side_nav").addClass('hide-menu');
+    $("body").css("overflow","unset");
+  }
+}
+
 $(function(){ $(".page-header").load("components/header.html") });
+$(function(){ $(".page-footer").load("components/footer.html") });
 
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
-      var banner = new Splide( '#banner' ,{type:'fade',heightRatio: 0.45,rewind:true,drag:false,autoplay:true,pauseOnHover:false,pagination:false,arrows:false}).mount();
       load_img();
+
+      $(".menu_ico").click(function(){ toogle_side_nav(); });
+      $("[button]").click(function(){ load_page($(this).attr("button")); })
+    
+      $(document).mouseup(function(e) 
+      {
+          var container_2 = $(".side_nav_container");
+          if (!container_2.is(e.target) && container_2.has(e.target).length === 0) 
+          {
+            var container_3 = $(".menu_ico");
+            if (!container_3.is(e.target) && container_3.has(e.target).length === 0) 
+            {
+              if ($(".side_nav").hasClass('show-menu')) {
+                $(".side_nav").removeClass('show-menu');
+                $(".side_nav").addClass('hide-menu');
+                $("body").css("overflow","unset");
+              }
+            }
+          }
+      });
   }
 }
